9873e5659775e401090a5984af50288177c6d812
karolkrzych <karolkrzych@gmail.com>
2019-07-30 13:21:31 +0200

    Wyświetlanie powiadomień dla użytkownika

--- PortalRandkowy.SPA/src/app/nav/nav.component.ts
+++ PortalRandkowy.SPA/src/app/nav/nav.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../_services/auth.service';
+import { AlertifyService } from '../_services/alertify.service';
+
 
 @Component({
   selector: 'app-nav',
@@ -10,27 +12,27 @@ export class NavComponent implements OnInit {
 
   model: any = {};
 
-  constructor(private authService: AuthService) { }
+  constructor(private authService: AuthService, private alertify: AlertifyService) { }
 
   ngOnInit() {
   }
 
-  login(){
-    this.authService.login(this.model).subscribe(next =>{
-      console.log('Zalogowałeś sę do aplikacji');
-    }, error =>{
-      console.log('Wystąpił błąd logowania');
+  login() {
+    this.authService.login(this.model).subscribe(next => {
+      this.alertify.success('Zalogowałeś sę do aplikacji');
+    }, error => {
+      this.alertify.error('Wystąpił błąd logowania');
     });
   }
 
-  loggedIn(){
+  loggedIn() {
     const token = localStorage.getItem('token');
     return !!token;
   }
 
-  logout(){
+  logout() {
     localStorage.removeItem('token');
-    console.log('Zostałeś wylogowany');
+    this.alertify.message('Zostałeś wylogowany');
   }
 
 }

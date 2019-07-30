322793ed4c96c80e5cfa1fe1addf83280b73ed2c
karolkrzych <karolkrzych@gmail.com>
2019-07-30 18:12:38 +0200

    Dodanie Dto

---
+++ PortalRandkowy.API/Dtos/UserForDetailedDto.cs
@@ -0,0 +1,62 @@
+using System;
+using System.Collections.Generic;
+using PortalRandkowy.API.Models;
+
+namespace PortalRandkowy.API.Dtos
+{
+    public class UserForDetailedDto
+    {
+        public int Id { get; set; }
+        public string Username { get; set; }
+
+        // Podstawowe informacje
+        
+        public string Gender { get; set; }
+        public int Age { get; set; }
+        public string ZodiacSign { get; set; }
+        public DateTime Created { get; set; }
+        public DateTime LastActive { get; set; }
+        public string City { get; set; }
+        public string Country { get; set; }
+
+        // Zakładka Info
+
+        public string Height { get; set; }
+        public string EyeColor { get; set; }
+        public string HairColour { get; set; }
+        public string MartialStatus { get; set; }
+        public string Education { get; set; }
+        public string Profession { get; set; }
+        public string Children { get; set; }
+        public string Languages { get; set; }
+
+        // Zakładka O mnie
+
+        public string Motto { get; set; }
+        public string Description { get; set; }
+        public string Personality { get; set; }
+        public string LookingFor { get; set; }
+
+
+        // Zakładka Pasje, zainteresowania
+
+        public string Interests { get; set; }
+        public string FreeTime { get; set; }
+        public string Sport { get; set; }
+        public string Movies { get; set; }
+        public string Music { get; set; }
+
+        // Zakładka Preferencje
+
+        public string ILike { get; set; }
+        public string IdoNotLike { get; set; }
+        public string MakesMeLaugh { get; set; }
+        public string ItFeelsBestIn { get; set; }
+        public string FriendsWouldDescribeMe { get; set; }
+
+        // Zakładka Zdjęcia
+
+        public ICollection<Photo> Photos { get; set; }
+        public string PhotoUrl { get; set; }
+    }
+}

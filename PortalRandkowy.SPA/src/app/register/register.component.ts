136edbb5b28826f7e079833fe58b0d094fc4ae9e
karolkrzych <karolkrzych@gmail.com>
2019-07-30 12:47:47 +0200

    Metoda rejestracji użytkownika

--- PortalRandkowy.SPA/src/app/register/register.component.ts
+++ PortalRandkowy.SPA/src/app/register/register.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit, Input, EventEmitter, Output } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter} from '@angular/core';
+import { AuthService } from '../_services/auth.service';
 
 @Component({
   selector: 'app-register',
@@ -7,20 +8,23 @@ import { Component, OnInit, Input, EventEmitter, Output } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
-  @Input() valuesFromHome: any;
   @Output() cancelRegister = new EventEmitter();
   model: any = {};
 
-  constructor() { }
+  constructor(private authService: AuthService) { }
 
   ngOnInit() {
   }
 
-  register(){
-    console.log(this.model);
+  register() {
+    this.authService.register(this.model).subscribe(() => {
+      console.log('rejestracja udana');
+    }, error => {
+      console.log(error);
+    });
   }
 
-  cancel(){
+  cancel() {
     this.cancelRegister.emit(false);
     console.log('Anulowane');
   }

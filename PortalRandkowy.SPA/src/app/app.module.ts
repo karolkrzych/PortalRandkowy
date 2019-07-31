26bd321cc7ac95c3cd897e0abc9f400c189c4973
karolkrzych <karolkrzych@gmail.com>
2019-07-31 16:45:58 +0200

    Dodanie komponentu karta użytkownika

--- PortalRandkowy.SPA/src/app/app.module.ts
+++ PortalRandkowy.SPA/src/app/app.module.ts
@@ -19,6 +19,7 @@ import { MessagesComponent } from './messages/messages.component';
 import { appRoutes } from './routes';
 import { AuthGuard } from './_guards/auth.guard';
 import { ErrorInterceptor, ErrorInterceptorProvider } from './_services/error.interceptor';
+import { UserCardComponent } from './users/user-card/user-card.component';
 
 
 
@@ -35,7 +36,8 @@ export function tokenGetter() {
       RegisterComponent,
       UserListComponent,
       LikesComponent,
-      MessagesComponent
+      MessagesComponent,
+      UserCardComponent
    ],
    imports: [
       BrowserModule,

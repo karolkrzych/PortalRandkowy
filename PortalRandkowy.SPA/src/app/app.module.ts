dc48065d94f43f4502ab37573dc7b832109dec33
karolkrzych <karolkrzych@gmail.com>
2019-07-31 16:25:59 +0200

    Obsługa błędów Angular

--- PortalRandkowy.SPA/src/app/app.module.ts
+++ PortalRandkowy.SPA/src/app/app.module.ts
@@ -18,6 +18,7 @@ import { LikesComponent } from './likes/likes.component';
 import { MessagesComponent } from './messages/messages.component';
 import { appRoutes } from './routes';
 import { AuthGuard } from './_guards/auth.guard';
+import { ErrorInterceptor, ErrorInterceptorProvider } from './_services/error.interceptor';
 
 
 
@@ -54,7 +55,8 @@ export function tokenGetter() {
       AuthService,
       AlertifyService,
       UserService,
-      AuthGuard
+      AuthGuard,
+      ErrorInterceptorProvider
    ],
    bootstrap: [
       AppComponent
--- PortalRandkowy.SPA/src/app/register/register.component.ts
+++ PortalRandkowy.SPA/src/app/register/register.component.ts
@@ -21,7 +21,7 @@ export class RegisterComponent implements OnInit {
     this.authService.register(this.model).subscribe(() => {
       this.alertify.success('Rejestracja udana');
     }, error => {
-      this.alertify.error('Wystąpił błąd rejestracji');
+      this.alertify.error(error);
     });
   }
 

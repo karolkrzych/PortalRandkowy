61bf8433a0a32636d4dac874f8025e34473dcf16
karolkrzych <karolkrzych@gmail.com>
2019-08-01 10:29:42 +0200

    Galeria zdjęć

--- PortalRandkowy.SPA/src/app/users/user-detail/user-detail.component.ts
+++ PortalRandkowy.SPA/src/app/users/user-detail/user-detail.component.ts
@@ -3,6 +3,7 @@ import { User } from 'src/app/_models/user';
 import { UserService } from 'src/app/_services/user.service';
 import { AlertifyService } from 'src/app/_services/alertify.service';
 import { ActivatedRoute } from '@angular/router';
+import { NgxGalleryOptions, NgxGalleryImage, NgxGalleryAnimation } from 'ngx-gallery';
 
 @Component({
   selector: 'app-user-detail',
@@ -12,24 +13,44 @@ import { ActivatedRoute } from '@angular/router';
 export class UserDetailComponent implements OnInit {
 
   user: User;
+  galleryOptions: NgxGalleryOptions[];
+  galleryImages: NgxGalleryImage[];
 
   constructor(private userService: UserService,
-              private alertify: AlertifyService,
-              private route: ActivatedRoute) {}
+    private alertify: AlertifyService,
+    private route: ActivatedRoute) { }
 
   ngOnInit() {
     this.route.data.subscribe(data => {
       this.user = data.user;
     });
-  }
-                // users/3
-  // loadUser() {
-  //   this.userService.getUser(+this.route.snapshot.params.id)
-  //       .subscribe((user: User) => {
-  //         this.user = user;
-  //       }, error => {
-  //         this.alertify.error(error);
-  //       });
-  // }
 
+    this.galleryOptions = [
+      {
+        width: '600px',
+        height: '400px',
+        thumbnailsColumns: 4,
+        imagePercent: 100,
+        preview: false,
+        imageAnimation: NgxGalleryAnimation.Slide
+      },
+    ];
+
+    this.galleryImages = this.getImages();
+  }
+
+  getImages() {
+    const imageUrls = [];
+
+    // tslint:disable-next-line: prefer-for-of
+    for (let i = 0; i < this.user.photos.length; i++) {
+      imageUrls.push({
+        small: this.user.photos[i].url,
+        medium: this.user.photos[i].url,
+        big: this.user.photos[i].url,
+        description: this.user.photos[i].description
+      });
+    }
+    return imageUrls;
+  }
 }
